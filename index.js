add879abd30383c68ae2b2794513f7888b22df1c
ILYA <106765406+D15ND@users.noreply.github.com>
2025-07-01 20:26:56 +0300

    feat: add song button and js solution for relize this button

---
+++ index.js
@@ -0,0 +1,13 @@
+const btn = document.getElementById('playBtn');
+const audio = document.getElementById('cook__audio');
+
+btn.addEventListener('click', () => {
+  audio.play();
+  btn.textContent = "Relax";
+  btn.style.padding = "5px 18px";
+});
+
+audio.addEventListener('ended', () => {
+  btn.textContent = "Play";
+  btn.style.padding = "5px 25px";
+})
